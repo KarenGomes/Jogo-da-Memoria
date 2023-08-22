5870540deef47e466d5630a2aa71657776fd7f39
KarenGomes <karenecris12015@gmail.com>
2023-08-22 15:57:06 -0300

    game development

---
+++ assets/js/game.js
@@ -0,0 +1,38 @@
+const grid = document.querySelector('.grid');
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const characters = [
+    'Amethyst',
+    'garnet',
+    "giant woman",
+    'lapis',
+    'pearl', 
+    'peridot',
+    'pink',
+    'ruby',
+    'sapphire',
+    'steven',
+];
+
+const createCard = () => {
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');    
+    const back = createElement('div', 'face back');
+
+    card.appendChild(front);
+    card.appendChild(back);
+}
+
+const loadGame = () => {
+    characters.forEach((character)=>{
+        const card = createCard();
+        grid.appendChild(card);
+    });
+}
+
+loadGame();
