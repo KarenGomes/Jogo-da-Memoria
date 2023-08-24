9289258d8b0d804ace21d326439452764befe9b7
KarenGomes <karenecris12015@gmail.com>
2023-08-24 11:14:47 -0300

    game development

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,36 +1,40 @@
 const grid = document.querySelector('.grid');
 
-const createElement = (tag, className) => {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
-}
-
 const characters = [
     'Amethyst',
     'garnet',
-    "giant woman",
+    "giant-woman",
     'lapis',
     'pearl', 
     'peridot',
     'pink',
     'ruby',
     'sapphire',
-    'steven',
+    'steven'    
 ];
 
-const createCard = () => {
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createCard = (character) => {
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');    
     const back = createElement('div', 'face back');
 
+    front.style.backgroundImage = `url('../images/${character}.jpg')`;
+
     card.appendChild(front);
     card.appendChild(back);
+
+    return card;
 }
 
 const loadGame = () => {
     characters.forEach((character)=>{
-        const card = createCard();
+        const card = createCard(character);
         grid.appendChild(card);
     });
 }
