32ba1c8438cebabd851041024056cc4d1ebc1404
KarenGomes <karenecris12015@gmail.com>
2023-08-29 13:30:51 -0300

    game development

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,7 +35,7 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');    
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../images/${character}.jpg')`;
+    front.style.backgroundImage = `url('../images/${character}.png')`;
 
     card.appendChild(front);    
     card.appendChild(back);
